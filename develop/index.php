5db8d5183d81eabef3ec3c7d52bf850dd9ee09d0
Roman <email.rom.melnyk@gmail.com>
2017-04-10 21:53:57 +0200

    Styling

--- develop/index.php
+++ develop/index.php
@@ -5,6 +5,38 @@
     <script type="application/javascript" src="./script.js"></script>
 </head>
 <body onload="IFTS.start();">
-    IFTS
+    <div class="main-content">
+        <header class="row">
+            <div class="column-3">
+                <div class="logo">/-- LOGO --/</div>
+            </div>
+            <div class="column-1">
+                <div class="phone-nums">
+                    <ul>
+                        <li>+380 (67) 111-2233</li>
+                        <li>+380 (67) 111-2233</li>
+                        <li>+380 (67) 111-2233</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+        <section>
+            /-- main content --/
+        </section>
+
+        <footer class="row">
+            <div class="column-1">
+                <div class="logo">IFTS</div>
+            </div>
+            <div class="column-2">
+                <div class="phone-nums">Repair SEO test</div>
+            </div>
+            <div class="column-1">
+                &copy; IFTS
+                Розробка: ...
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
