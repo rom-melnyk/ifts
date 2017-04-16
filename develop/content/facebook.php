5047913d1d58f4fd20a41a9921a0059d64330209
Roman <email.rom.melnyk@gmail.com>
2017-04-17 00:42:08 +0200

    Facebook: part 4

--- develop/content/facebook.php
+++ develop/content/facebook.php
@@ -6,46 +6,41 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/php/facebook-sdk-v5/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/php/fb-config.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/php/fb-scripts.php';
 
-$URL_PARAM_SAVE_TOKEN = "save-token";
+$SAVE_TOKEN_URL_PARAM = 'save-token';
+$SAVE_TOKEN_URL = "http://ifts.if.ua/page/facebook?$SAVE_TOKEN_URL_PARAM";
 
 $fb = new Facebook\Facebook($FB_CONFIG);
 $helper = $fb->getRedirectLoginHelper();
 
-if (isset($_GET[$URL_PARAM_SAVE_TOKEN])) {
+if (isset($_GET[$SAVE_TOKEN_URL_PARAM])) {
     save_login_token();
-    exit;
 } else {
     $token = read_token();
     if (!$token) {
-        render_login_part();
-        exit;
+        render_login_part($helper, $SAVE_TOKEN_URL);
+    } else {
+        $fb->setDefaultAccessToken($token);
+        try {
+            $response = $fb->get('/me'); // https://www.facebook.com/groups/453544624728446/
+        } catch(Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch(Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        $response_data = $response->getDecodedBody();
+        echo var_dump($response_data);
     }
 
-    $fb->setDefaultAccessToken($token);
-    try {
-        $response = $fb->get('/me'); // https://www.facebook.com/groups/453544624728446/
-    } catch(Facebook\Exceptions\FacebookResponseException $e) {
-        // When Graph returns an error
-        echo 'Graph returned an error: ' . $e->getMessage();
-        exit;
-    } catch(Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-    }
-
-    $response_data = $response->getDecodedBody();
-    echo var_dump($response_data);
 }
 
 // ------------------------ helper functions ------------------------
-function render_login_part() {
-    global $URL_PARAM_SAVE_TOKEN;
-    global $helper;
-
-    echo isset($helper);
-    $login_url = $helper->getLoginUrl("http://ifts.if.ua/page/facebook?$URL_PARAM_SAVE_TOKEN", ['email']);
-
+function render_login_part($helper, $redirect_url) {
+    $login_url = $helper->getLoginUrl($redirect_url, ['email']);
     echo "<a href=\"$login_url\">Log in with Facebook</a>";
 }
 
