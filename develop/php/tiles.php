50d74a4daf3f49b6622cf48e49ec1d3c4dcedce6
Roman <email.rom.melnyk@gmail.com>
2017-05-06 15:58:49 +0200

    Responsive CSS

--- develop/php/tiles.php
+++ develop/php/tiles.php
@@ -18,32 +18,52 @@ function read_file() {
 function render_tiles() {
     $content = read_file();
     $count = count($content);
+    $quadrant_size = 0;
+
     for ($i = 0; $i < $count; $i++) {
         $object = $content[$i];
+
+        if ($quadrant_size === 0) {
+            echo '<div class="quadrant">';
+        }
+
+        $size = 1;
+        if (array_key_exists('size', $object)) {
+            $size = $object['size'];
+        }
+        $quadrant_size += $size;
+
+        $width = $size > 1 ? 2 : 1;
+        $height = $size === 4 ? 2 : 1;
+        echo "<div class=\"tile width-$width height-$height\">";
+
         $wrapper = get_wrapper($object);
-        $width = rand(1, 4);
-        echo "<div class=\"tile column-$width\">";
         echo $wrapper['open'];
         echo get_icon($object);
         echo get_title($object);
         echo $wrapper['close'];
         echo '</div>';
+
+        if ($quadrant_size >= 4) {
+            echo '</div>';
+            $quadrant_size = 0;
+        }
     }
 }
 
 // ----------------------- helpers -----------------------
 function get_title($object) {
     return array_key_exists('title', $object)
-        ? "<div class=\"title\">${object['title']}</div>"
+        ? '<div class="title">' . $object['title'] . '</div>'
         : '';
 }
 
 function get_icon($object) {
     global $ICONS_BASE;
     if (array_key_exists('icon-file', $object)) {
-        return "<div class=\"icon file\" style=\"background-image: url('${ICONS_BASE}${object['icon-file']}')\"></div>";
+        return '<div class="icon file" style="background-image: url(' . "'" . $ICONS_BASE . $object['icon-file'] . "'" . ')"></div>';
     } else if (array_key_exists('icon-class', $object)) {
-        return "<i class=\"icon fa ${object['icon-class']}\"></i>";
+        return '<i class="icon fa ' . $object['icon-class'] . '"></i>';
     } else {
         return '';
     }
@@ -71,7 +91,7 @@ function get_wrapper($object) {
 
     return $href
         ? array(
-            'open' => "<a href=\"$href\" class=\"inner-wrapper\" title=\"$link_title\">",
+            'open' => '<a href="' . $href . '" class="inner-wrapper" title="' . $link_title . '">',
             'close' => '</a>'
         )
         : array('open' => '<div class="inner-wrapper">', 'close' => '</div>');
