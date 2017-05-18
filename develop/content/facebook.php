2bc89dab52cb770a53e7ed290175cdf9ca9b2f5d
Roman <email.rom.melnyk@gmail.com>
2017-05-18 23:30:22 +0200

    FB v5 fixed

--- develop/content/facebook.php
+++ develop/content/facebook.php
@@ -2,15 +2,26 @@
 session_start();
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/php/fb-config.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/php/facebook-sdk-v3/facebook.php';
+
+// Facebook requires PHP v5.4+
+if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+    echo '<!--PHP v' . PHP_VERSION . ' -->';
+    echo "<script>location.href = '$FB_GROUP_PAGE';</script>";
+    exit();
+}
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/php/facebook-sdk-v5/autoload.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/php/fb-helpers.php';
+
+
+$THIS_PAGE = 'http://ifts.if.ua/page/facebook';
+$SAVE_TOKEN_URL_PARAM = 'save-token';
+$SAVE_TOKEN_URL = "$THIS_PAGE?$SAVE_TOKEN_URL_PARAM";
 
 $TOKEN_FILENAME = $_SERVER['DOCUMENT_ROOT'] . '/php/fb-token.txt';
 
-$fb = new Facebook(array(
-    'appId' => $FB_CONFIG['app_id'],
-    'secret' => $FB_CONFIG['app_secret']
-));
+$fb = new Facebook\Facebook($FB_CONFIG);
+$fb_helper = $fb->getRedirectLoginHelper();
 
 
 render_fb_part();
@@ -18,49 +29,64 @@ render_fb_part();
 
 // ------------------------ main ------------------------
 function render_fb_part() {
-    global $fb;
     global $SAVE_TOKEN_URL_PARAM;
     global $TOKEN_FILENAME;
 
-    $token = file_exists($TOKEN_FILENAME) ? file_get_contents($TOKEN_FILENAME) : FALSE;
-    $fb->setAccessToken($token);
-    $user = $fb->getUser();
-    if ($user) {
-        $posts = get_posts_from_file();
-
-        $date_from_file = $posts ? get_first_post_date($posts) : new DateTime('2000-01-01');
-        $date_from_fb = FALSE;
-
-        if (!$posts || get_time_diff(new DateTime(), $date_from_file) > 0) {
-            $posts = get_posts_from_fb($token);
-            if ($posts) {
-                $date_from_fb = get_first_post_date($posts);
+    if (isset($_GET[$SAVE_TOKEN_URL_PARAM])) {
+        $token = get_login_token();
+        if (isset($token)) {
+            if ($token) {
+                if (file_put_contents($TOKEN_FILENAME, $token)) {
+                    render_saved_ok_part();
+                } else {
+                    log_error('Token', 'Unable to save token file');
+                    render_login_part();
+                }
+            } else {
+                render_login_part();
             }
+        } else {
+            log_error('Auth', 'Token not set');
+            render_login_part();
         }
 
-        if ($posts) {
-            render_posts($posts);
-            if ($date_from_fb && get_time_diff($date_from_file, $date_from_fb) > 0) {
-                save_posts_to_file($posts);
+    } else {
+        $token = file_exists($TOKEN_FILENAME) ? file_get_contents($TOKEN_FILENAME) : FALSE;
+        if ($token) {
+            $posts = get_posts_from_file();
+
+            $date_from_file = $posts ? get_first_post_date($posts) : new DateTime('2000-01-01');
+            $date_from_fb = FALSE;
+
+            if (!$posts || get_time_diff(new DateTime(), $date_from_file) > 0) {
+                $posts = get_posts_from_fb($token);
+                if ($posts) {
+                    $date_from_fb = get_first_post_date($posts);
+                }
+            }
+
+            if ($posts) {
+                render_posts($posts);
+                if ($date_from_fb && get_time_diff($date_from_file, $date_from_fb) > 0) {
+                    save_posts_to_file($posts);
+                }
+            } else {
+                render_login_part();
             }
         } else {
             render_login_part();
         }
-    } else {
-        render_login_part();
     }
 }
 
 
 // ------------------------ helper functions ------------------------
 function render_login_part() {
-    global $fb;
+    global $fb_helper;
     global $SAVE_TOKEN_URL;
     global $FB_GROUP_PAGE;
 
-    $login_url = $fb->getLoginUrl(array('scope' => 'email', 'redirect_uri' => $SAVE_TOKEN_URL));
-
-    render_fb_javascript();
+    $login_url = $fb_helper->getLoginUrl($SAVE_TOKEN_URL, array('email'));
     echo <<<HTML
 <a class="link" href="$login_url">Залогінься</a>
 в Facebook або перейди на нашу
@@ -78,28 +104,21 @@ function render_saved_ok_part() {
 
 function get_login_token() {
     global $fb;
-    global $TOKEN_FILENAME;
+    global $fb_helper;
     global $THIS_PAGE;
 
     $token = FALSE;
     try {
-        $token = $fb->getAccessToken();
-    } catch (FacebookApiException $e) {
+        $token = $fb_helper->getAccessToken();
+    } catch (Facebook\Exceptions\FacebookResponseException $e) {
         log_error('Graph API', $e->getMessage());
-    } catch (Exception $e) {
-        log_error('General', $e->getMessage());
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        log_error('SDK', $e->getMessage());
     }
 
     if ($token) {
-        try {
-            if ($fb->setExtendedAccessToken() !== FALSE) {
-                $token = $fb->getAccessToken('access_token');
-            } else {
-                $token = FALSE;
-            };
-        } catch (Exception $e) {
-            log_error('Graph API / setExtenedAccessToken', $e->getMessage());
-        }
+        $oAuth2Client = $fb->getOAuth2Client();
+        $token = $oAuth2Client->getLongLivedAccessToken($token);
     }
 
     return $token;
@@ -113,32 +132,34 @@ function get_posts_from_fb($token) {
     global $FB_API_CALL_CONFIG;
 
     try {
-        $response = $fb->api("/$FB_GROUP_ID/feed");
-    } catch (FacebookApiException $e) {
+        $fb_app = $fb->getApp();
+        $request = new Facebook\FacebookRequest(
+            $fb_app,
+            $token,
+            'GET',
+            "/$FB_GROUP_ID/feed",
+            $FB_API_CALL_CONFIG
+        );
+        $response = $fb->getClient()->sendRequest($request);
+    } catch (Facebook\Exceptions\FacebookResponseException $e) {
         log_error('Graph API', $e->getMessage());
         $response = FALSE;
-    } catch (Exception $e) {
-        log_error('General', $e->getMessage());
+    } catch (Facebook\Exceptions\FacebookSDKException $e) {
+        log_error('SDK', $e->getMessage());
         $response = FALSE;
     }
 
+    if (!$response) {
+        return $response;
+    }
+
     try {
+        $response = $response->getDecodedBody();
         $response = $response['data'];
     } catch (Exception $e) {
         log_error('Response', 'Unable to parse output data');
         $response = FALSE;
     }
     return $response;
-}
-
-function render_fb_javascript() {
-    global $FB_CONFIG;
-    global $FB_GROUP_ID;
-
-    echo <<<HTML
-<script>
-    Object.assign(IFTS.facebook, { appId: '${FB_CONFIG['app_id']}', groupId: '$FB_GROUP_ID' });
-</script>
-HTML;
 }
 ?>
