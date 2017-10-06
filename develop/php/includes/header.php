ac69dcb1def66c5ee01da1f1ce61a9dd31393906
Roman <email.rom.melnyk@gmail.com>
2017-10-06 21:57:34 +0200

    Header: address added

--- develop/php/includes/header.php
+++ develop/php/includes/header.php
@@ -10,6 +10,7 @@
             <li><i class="fa fa-phone"></i><b>(097)</b> 133-22-11</li>
             <li><i class="fa fa-phone"></i><b>(095)</b> 029-69-29</li>
             <li><i class="fa fa-envelope-o"></i><a class="link" href="mailto:whiplash@ukr.net">whiplash@ukr.net</a></li>
+            <li><i class="fa fa-map-marker"></i>м.&nbsp;Івано-Франківськ, вул.&nbsp;Вовчинецька,&nbsp;28</li>
         </ul>
     </div>
 </header>
